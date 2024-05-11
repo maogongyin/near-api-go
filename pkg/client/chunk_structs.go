d841d67758d1542db9d1068d9103829089be98d6
maogongyin <44475907@qq.com>
2024-05-11 19:24:05 +0800

    update chunk params, remove signature field because Near RPC return invalid public key in signature

--- pkg/client/chunk_structs.go
+++ pkg/client/chunk_structs.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/maogongyin/near-api-go/pkg/types"
 	"github.com/maogongyin/near-api-go/pkg/types/hash"
-	"github.com/maogongyin/near-api-go/pkg/types/signature"
 )
 
 type ChunkView struct {
@@ -16,22 +15,21 @@ type ChunkView struct {
 }
 
 type ChunkHeaderView struct {
-	ChunkHash            hash.CryptoHash           `json:"chunk_hash"`
-	PrevBlockHash        hash.CryptoHash           `json:"prev_block_hash"`
-	OutcomeRoot          hash.CryptoHash           `json:"outcome_root"`
-	PrevStateRoot        json.RawMessage           `json:"prev_state_root"` // TODO: needs a type!
-	EncodedMerkleRoot    hash.CryptoHash           `json:"encoded_merkle_root"`
-	EncodedLength        uint64                    `json:"encoded_length"`
-	HeightCreated        types.BlockHeight         `json:"height_created"`
-	HeightIncluded       types.BlockHeight         `json:"height_included"`
-	ShardID              types.ShardID             `json:"shard_id"`
-	GasUsed              types.Gas                 `json:"gas_used"`
-	GasLimit             types.Gas                 `json:"gas_limit"`
-	RentPaid             types.Balance             `json:"rent_paid"`        // TODO: deprecated
-	ValidatorReward      types.Balance             `json:"validator_reward"` // TODO: deprecated
-	BalanceBurnt         types.Balance             `json:"balance_burnt"`
-	OutgoingReceiptsRoot hash.CryptoHash           `json:"outgoing_receipts_root"`
-	TxRoot               hash.CryptoHash           `json:"tx_root"`
-	ValidatorProposals   []ValidatorStakeView      `json:"validator_proposals"`
-	Signature            signature.Base58Signature `json:"signature"`
+	ChunkHash            hash.CryptoHash      `json:"chunk_hash"`
+	PrevBlockHash        hash.CryptoHash      `json:"prev_block_hash"`
+	OutcomeRoot          hash.CryptoHash      `json:"outcome_root"`
+	PrevStateRoot        json.RawMessage      `json:"prev_state_root"` // TODO: needs a type!
+	EncodedMerkleRoot    hash.CryptoHash      `json:"encoded_merkle_root"`
+	EncodedLength        uint64               `json:"encoded_length"`
+	HeightCreated        types.BlockHeight    `json:"height_created"`
+	HeightIncluded       types.BlockHeight    `json:"height_included"`
+	ShardID              types.ShardID        `json:"shard_id"`
+	GasUsed              types.Gas            `json:"gas_used"`
+	GasLimit             types.Gas            `json:"gas_limit"`
+	RentPaid             types.Balance        `json:"rent_paid"`        // TODO: deprecated
+	ValidatorReward      types.Balance        `json:"validator_reward"` // TODO: deprecated
+	BalanceBurnt         types.Balance        `json:"balance_burnt"`
+	OutgoingReceiptsRoot hash.CryptoHash      `json:"outgoing_receipts_root"`
+	TxRoot               hash.CryptoHash      `json:"tx_root"`
+	ValidatorProposals   []ValidatorStakeView `json:"validator_proposals"`
 }
